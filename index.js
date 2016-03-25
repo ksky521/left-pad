fa3a8921648398f4d9a7c8fcf66bd1b71064e218
ksky521 <ksky521@gmail.com>
2016-03-25 14:49:40 +0800

    Update index.js
    玩一玩

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-module.exports = leftpad;
-
-function leftpad (str, len, ch) {
-  str = String(str);
-
-  var i = -1;
-
-  if (!ch && ch !== 0) ch = ' ';
-
-  len = len - str.length;
-
-  while (++i < len) {
-    str = ch + str;
+module.exports = function(str, len, ch) {
+  str = '' + str;
+  if (!ch && ch !== 0) ch = '';
+  var plen = len - str.length;
+  if (plen > 0) {
+    return new Array(plen + 1).join(ch) + str;
   }
-
-  return str;
+  return str.slice(-len);
 }
